286bbc36659aa782bbb7dd0db489456c6b622631
Lexa M <lexa-m@users.noreply.github.com>
2017-08-21 18:07:07 +0700

    CCToken moderated tests.

---
+++ test/Moderated.js
@@ -0,0 +1,80 @@
+'use strict';
+const assertJump = require('./helpers/assertJump');
+
+var Moderated = artifacts.require('../token/ownership/Moderated.sol');
+
+contract('Moderated', function(accounts) {
+  var moderated;
+
+  before(async function() {
+    moderated = await Moderated.new();
+  });
+
+  it('should moderator == owner after creation', async function() {
+    let moderator = await moderated.moderator();
+    let owner = await moderated.owner();
+    assert.isTrue(moderator == owner);
+  });
+
+  it('should change moderator', async function() {
+    let other = accounts[1];
+    let owner = await moderated.owner();
+    let moderator = await moderated.moderator();
+
+    assert.isTrue(moderator === owner);
+
+    // change moderator
+    await moderated.transferModeratorship(other, {from: moderator});
+    let newModerator = await moderated.newModerator();
+    assert.isTrue(newModerator === other);
+
+    // approve moderator
+    await moderated.approveModeratorship({from: other});
+    let approvedModerator = await moderated.moderator();
+    assert.isTrue(approvedModerator === other);
+  });
+
+  it('should prevent non-moderator from changing moderator', async function() {
+    let owner = await moderated.owner();
+    let moderator = await moderated.moderator();
+
+    assert.isTrue(moderator != owner);
+    try {
+      await moderated.transferModeratorship(owner, {from: owner});
+      assert.fail('should have thrown before');
+    } catch(error) {
+      assertJump(error);
+    }
+  });
+
+  it('only owner can remove moderator', async function() {
+    let owner = await moderated.owner();
+    let moderator = await moderated.moderator();
+    assert.isTrue(moderator != owner);
+
+    try {
+      await moderated.removeModeratorship({from: moderator});
+      assert.fail('should have thrown before');
+    } catch(error) {
+      assertJump(error);
+    }
+
+    await moderated.removeModeratorship({from: owner});
+    let hasModerator = await moderated.hasModerator.call();
+    assert.isFalse(hasModerator);
+  });
+
+/*
+  it('only moderator can do transfer', async function() {
+    let owner = await moderated.owner();
+
+    try {
+      await moderated.transferFrom(accounts[1], accounts[2], 200000, {from: owner});
+      assert.fail('should have thrown before');
+    } catch(error) {
+      assertJump(error);
+    }
+  });
+*/
+
+});
