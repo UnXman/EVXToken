33ec71f084aef1b7033d01ed925d7f49d991d87d
Artem <artemk@211.ru>
2017-09-11 14:33:07 +0700

    transferFrom -> moderatorTransferFrom

--- token/token/evxPausableToken.sol
+++ token/token/evxPausableToken.sol
@@ -33,7 +33,23 @@ contract evxPausableToken is StandardToken, evxPausable {
     return super.transfer(_to, _value);
   }
 
-  function transferFrom(address _from, address _to, uint256 _value) onlyModerator returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) whenNotPaused returns (bool) {
+    require(!isFreezed(msg.sender));
+    require(!isFreezed(_from));
+    require(!isFreezed(_to));
     return super.transferFrom(_from, _to, _value);
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function moderatorTransferFrom(address _from, address _to, uint256 _value) onlyModerator returns (bool) {
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
   }
 }
