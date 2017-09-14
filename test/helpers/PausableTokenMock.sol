c80095a511acdacfd13f797007e6855f7d88ebad
Artem <artemk@211.ru>
2017-09-14 18:16:41 +0700

    Original tests over EVXToken contract added

--- test/helpers/PausableTokenMock.sol
+++ test/helpers/PausableTokenMock.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.11;
 import '../../contracts/token/evxModeratedToken.sol';
 
 // mock class using PausableToken
-contract evxModeratedTokenMock is evxModeratedToken {
+contract PausableTokenMock is evxModeratedToken{
 
-  function evxModeratedTokenMock(address initialAccount, uint initialBalance) {
+  function PausableTokenMock(address initialAccount, uint initialBalance) {
     balances[initialAccount] = initialBalance;
   }
 
