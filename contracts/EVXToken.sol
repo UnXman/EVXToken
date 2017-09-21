9f011c3103b1e8125b8766d83e170d2a979bdda8
Artem <artemk@211.ru>
2017-09-21 20:04:25 +0700

    Changes reverted

--- contracts/EVXToken.sol
+++ contracts/EVXToken.sol
@@ -9,19 +9,13 @@ contract EVXToken is evxModeratedToken {
   string public constant name = "Everex";
   string public constant symbol = "EVX";
   uint256 public constant decimals = 4;
-  uint256 initialSupply = 250000000000;
 
   /**
    * @dev Contructor that gives msg.sender all of existing tokens. 
    */
   function EVXToken(uint256 _initialSupply) {   
-    if(_initialSupply > 0){
-        initialSupply = _initialSupply;
-    }
-    totalSupply = initialSupply;
-    balances[msg.sender] = initialSupply;
-    // Contract initializes in paused state
-    // paused = true;
+    totalSupply = _initialSupply;
+    balances[msg.sender] = _initialSupply;
   }
 }
 
