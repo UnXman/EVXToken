df50415948e6ea58e851d30e558a57b7201723db
Artem <artemk@211.ru>
2017-09-21 17:47:31 +0700

    initialSupply const error fixed

--- contracts/EVXToken.sol
+++ contracts/EVXToken.sol
@@ -9,7 +9,7 @@ contract EVXToken is evxModeratedToken {
   string public constant name = "Everex";
   string public constant symbol = "EVX";
   uint256 public constant decimals = 4;
-  uint256 public constant initialSupply = 250000000000;
+  uint256 initialSupply = 250000000000;
 
   /**
    * @dev Contructor that gives msg.sender all of existing tokens. 
