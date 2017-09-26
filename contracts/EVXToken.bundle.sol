834b4996e51ca83e35c4da8e314e2fe1e4b5c497
Artem <artemk@211.ru>
2017-09-26 20:23:50 +0700

    freezed -> frozen

--- contracts/EVXToken.bundle.sol
+++ contracts/EVXToken.bundle.sol
@@ -391,53 +391,53 @@ contract evxPausable is Pausable, evxModerated {
  **/
 contract evxModeratedToken is StandardToken, evxPausable {
 
-  mapping(address => bool) freezed;
+  mapping(address => bool) frozen;
 
   /**
-   * @dev Check if given address is freezed. Freeze works only if moderator role is active
+   * @dev Check if given address is frozen. Freeze works only if moderator role is active
    * @param _addr address Address to check
    */
-  function isFreezed(address _addr) constant returns (bool){
-      return freezed[_addr] && hasModerator();
+  function isFrozen(address _addr) constant returns (bool){
+      return frozen[_addr] && hasModerator();
   }
 
   /**
    * @dev Freezes address (no transfer can be made from or to this address).
-   * @param _addr address Address to be freezed
+   * @param _addr address Address to be frozen
    */
   function freeze(address _addr) onlyModerator {
-      freezed[_addr] = true;
+      frozen[_addr] = true;
   }
 
   /**
-   * @dev Unfreezes freezed address.
-   * @param _addr address Address to be unfreezed
+   * @dev Unfreezes frozen address.
+   * @param _addr address Address to be unfrozen
    */
   function unfreeze(address _addr) onlyModerator {
-      freezed[_addr] = false;
+      frozen[_addr] = false;
   }
 
   /**
-   * @dev Declines transfers from/to freezed addresses.
+   * @dev Declines transfers from/to frozen addresses.
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amout of tokens to be transfered
    */
   function transfer(address _to, uint256 _value) whenNotPaused returns (bool) {
-    require(!isFreezed(msg.sender));
-    require(!isFreezed(_to));
+    require(!isFrozen(msg.sender));
+    require(!isFrozen(_to));
     return super.transfer(_to, _value);
   }
 
   /**
-   * @dev Declines transfers from/to/by freezed addresses.
+   * @dev Declines transfers from/to/by frozen addresses.
    * @param _from address The address which you want to send tokens from
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amout of tokens to be transfered
    */
   function transferFrom(address _from, address _to, uint256 _value) whenNotPaused returns (bool) {
-    require(!isFreezed(msg.sender));
-    require(!isFreezed(_from));
-    require(!isFreezed(_to));
+    require(!isFrozen(msg.sender));
+    require(!isFrozen(_from));
+    require(!isFrozen(_to));
     return super.transferFrom(_from, _to, _value);
   }
 
